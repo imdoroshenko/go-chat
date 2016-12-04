d38d64ec32c4e7eea0db0ee0ce2e87073dfa1d33
Ivan Doroshenko <ivan.doroshenko@gettyimages.com>
2016-12-04 21:25:51 +0200

    channels

---
+++ README.md
@@ -0,0 +1,14 @@
+#GO-CHAT with channels
+
+##Usage
+
+```go get github.com/imdoroshenko/go-chat```
+
+```cd $HOPATH/github.com/imdoroshenko/go-chat```
+
+```go run *.go```
+
+Open through you local web server or directly:
+```open static/index.html```
+
+
---
+++ channel-manager.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+)
+
+type ChannelManager struct {
+	Channels map[string]*appChannel
+	Open chan string
+	Close chan string
+}
+
+func (cm *ChannelManager) Run() {
+	for {
+		select {
+		case name := <-cm.Open:
+			cm.Channels[name] = newChannel(name)
+			go cm.Channels[name].Run()
+			log.Printf("%s channel is open\n", name)
+		case name := <-cm.Close:
+			cm.Channels[name].Stop <- 1
+			delete(cm.Channels, name)
+			log.Printf("%s channel is closed\n", name)
+		}
+	}
+}
+
+//
+//func (cm *ChannelsManager) TransferClients(from string, to string) err {
+//	if fromCh, ok := cm.Channels[from]; ok {
+//		return fmt.Errorf("%s channel does not exist", from)
+//	}
+//	if toCh, ok := cm.Channels[from]; ok {
+//		return fmt.Errorf("%s channel does not exist", to)
+//	}
+//	for client, _ := range fromCh.clients {
+//		fromCh.le
+//	}
+//}
+
+func NewChannelManager() *ChannelManager {
+	return &ChannelManager{
+		Channels: make(map[string]*appChannel),
+		Open:   make(chan string),
+		Close:   make(chan string),
+	}
+}
